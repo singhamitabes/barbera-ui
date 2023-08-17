21cd2a043c5f7f728f2b2b1a440fabd754ff2b3f
Amit Sing <singh.amitabes@gmail.com>
2023-08-17 09:47:00 +0530

    first commit

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <>
       <div className="d-flex header">
-        <div className="col-sm header float-right ">
+        <div className="col-sm header float-right">
           <label className="">Opening Hour :  Sunday - Friday , 08:00 am - 10:00 pm</label>
         </div>
         <div className="col-sm d-flex justify-content-center">
@@ -16,28 +16,33 @@ export default function Navbar() {
           <label className="px-1"><FaLinkedin /></label>
         </div>
       </div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-dark text-light d-flex justify-content-start ">
-        <div className="div bg-dark mx-5 halfWidth">
-          <img src="/Assets/images/barbera_logo.png" alt="image" />
-        </div>
-        <div className="div halfWidth">
-          <ul className="navbar-nav mx-5 d-flex justify-content-between ">
-            <li className="nav-item active">
-              <Link className="nav-link text-white" to="/">Home </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white" to="/aboutus">AboutUs</Link>
-            </li>
-            <li className="nav-item active">
-              <Link className="nav-link text-white" to="/services">Service </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white" to="contactus">Contact</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white" to="getanapp">Get The App</Link>
-            </li>
-          </ul>
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark text-light">
+        <div className="container">
+          <div className="navbar-brand">
+            <img src="/Assets/images/barbera_logo.png" alt="image" />
+          </div>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/aboutus">About Us</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/services">Service</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/contactus">Contact</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/getanapp">Get The App</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
